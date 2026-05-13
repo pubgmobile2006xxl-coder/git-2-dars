aaee1d07efd7b32c75649a4f8921843dac11115d
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-13 21:28:49 +0500

    cdcdf

--- vite-project/src/Cmpanents/Header/Header.jsx
+++ vite-project/src/Cmpanents/Header/Header.jsx
@@ -1,21 +1,28 @@
-import React from "react"
+import React, { useTranslation } from "react"
 import "./Header.css"; 
 import frame from "../../assets/Frame.png";  
+import { useTranslation } from "react-i18next";
+import i18n from "../../i18n";
 
  const Header = () =>{
+    const {t,i18n} = useTranslation()
+
+    const handleCHangeLanguage = (event)=>{
+        i18n.changeLanguage(event.target.value)
+    }
     return (
         <header>
             <div className="container">
                 <div className="navbar">
                     <img src={frame} alt="rasm"/>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Work</a></li>
-                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">{t("header.Home")}</a></li>
+                        <li><a href="#">{t("header.About")}t</a></li>
+                        <li><a href="#">{t("header.Services")}</a></li>
+                        <li><a href="#">{t("header.Work")}</a></li>
+                        <li><a href="#">{t("header.Blog")}</a></li>
                     </ul>
-                    <select name="" id="">
+                    <select name="" id="" onChange={handleCHangeLanguage}>
                         <option value="uzb">uzb</option>
                         <option value="rus">rus</option>
                         <option value="eng">eng</option>
